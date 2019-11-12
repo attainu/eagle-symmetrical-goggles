9ac8739794fa5999d4b36fdecb486162d4fa7886
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-11-12 21:59:31 +0530

    hbs config and installed packages

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const PORT = 9090;
+
+app.use(express.json());
+app.use(express.urlencoded())
+
+// Configure Handlebars
+const hbs = exphbs.create({
+	extname: '.hbs'
+});
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+
+app.get('/', function(req,res) {
+	res.render('home');
+})
+
+// Start the app on pre defined port number
+app.listen(PORT, function() {
+	console.log("Application has started and running on port: ", PORT);
+}).on('error', function(error) {
+	console.log("Unable to start app. Error >>>>", error);
+});
