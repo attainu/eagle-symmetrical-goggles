ac82d8b4ce14f30d42302bdda1ba6de1f8b3dadb
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-11-14 22:26:37 +0530

    search for posts done/ will be rendered on UI later

---
+++ controllers/search.js
@@ -0,0 +1,26 @@
+const SearchController =  {};
+const Model = require('./../models/Search.js');
+
+SearchController.search = function(req, res) {
+    var term = req.params.term;
+    var db = req.app.locals.db;
+    Model.search(term, db, function(error, data) {
+        if(error) {
+			return res.status(500).json(
+				{
+					status: false,
+					error
+				}
+			);
+        }
+        return res.status(200).json(
+			{
+				status: true,
+				data
+            }
+        )
+
+    })
+}
+
+module.exports = SearchController;
