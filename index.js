4135f787825b7b31d35acbf76ef5d595f35fc214
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-11-14 16:31:47 +0530

    checkIfLoggedIn middleware done

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ var session = require('express-session')
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(authRoute.checkIfLoggedIn);
 app.use(express.static('public'))
 
 //session config
 app.use(session({
 	name: 'Somename',
 	secret: 'adfasdfas',
-	resave : false,
+	resave : true,
 	saveUninitialized: true,
 	cookie: {
 		httpOnly: true,
@@ -55,8 +54,11 @@ app.get('/signup', function(req,res) {
 app.get('/about', function(req,res) {
 	res.render('about', {title: 'About Us'});
 });
+app.use(authRoute.checkIfLoggedIn);
 app.post('/login', authRoute.login);
 app.post('/logout', authRoute.logout);
+
+
 
 // Start the app on pre defined port number
 app.listen(PORT, function() {
