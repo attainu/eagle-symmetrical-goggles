8c6e40efad0df49766b6970572a12d69b29cf390
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-11-27 20:07:35 +0530

    first heroku commit

--- models/index.js
+++ models/index.js
@@ -10,7 +10,8 @@ if(process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'staging') {
     const DB_USERNAME = process.env.DB_USERNAME;
     const DB_PASSWORD = process.env.DB_PASSWORD;
     const DB_URL = process.env.DB_URL;
-    var dbURL = 'mongodb+srv://'+DB_USERNAME+':'+DB_PASSWORD+'@'+DB_URL;
+    //var dbURL = 'mongodb+srv://'+DB_USERNAME+':'+DB_PASSWORD+'@'+DB_URL;
+    var dbURL = 'mongodb+srv://root:whitecollar@whitecollar-k9pft.mongodb.net/test?retryWrites=true&w=majority'
 }else {
     var dbURL = 'mongodb://localhost:27017/whitecollardb'
 }
